5083f1fa08830381048a8f44e220617e2922b41d
adventuresofboy-admin <noeolivarez97@gmail.com>
2023-07-09 21:31:45 -0500

    Add project files.

---
+++ AceBackEnd/Controllers/LoginController.cs
@@ -0,0 +1,67 @@
+﻿using AceBackEnd.Data_Transfer_Objects;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic;
+using System.Runtime.InteropServices.ObjectiveC;
+using System.Runtime.Serialization;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AceBackEnd.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        // GET: api/<LoginController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<LoginController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+
+   
+
+        [HttpPost]
+       public async Task<IActionResult> LoginEndpoint([FromBody] LoginDTO dtoObject)
+        {
+            try
+            {
+                LoginDTO returnObject =new  LoginDTO();
+                returnObject = dtoObject;
+
+            return Ok(returnObject);
+            }
+            catch(Exception ex)
+            {
+                return Ok(ex.Message);
+            }
+        }
+
+        // POST api/<LoginController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<LoginController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<LoginController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ AceBackEnd/Data Transfer Objects/LoginDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace AceBackEnd.Data_Transfer_Objects
+{
+    public class LoginDTO
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }    
+      
+
+    }
+}
