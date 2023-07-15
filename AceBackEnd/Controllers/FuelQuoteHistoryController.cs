c0a6ef4db3133560910eb62f122b3ec7c34ff474
Elijah Ochoa <elijah.8888@yahoo.com>
2023-07-14 21:08:49 -0600

    all tests passed

--- AceBackEnd/Controllers/FuelQuoteHistoryController.cs
+++ AceBackEnd/Controllers/FuelQuoteHistoryController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.VisualBasic;
 using System.Runtime.InteropServices.ObjectiveC;
 using System.Runtime.Serialization;
+using System.Text.Json.Nodes;
 
 namespace AceBackEnd.Controllers
 {
@@ -38,5 +39,6 @@ namespace AceBackEnd.Controllers
                 return StatusCode(500, "A problem happened while handling your request.");
             }
         }
+        
     }
 }
